67242d0484cb6c109ac0da308cf1409e1e68f388
saliow <nemusak35@gmail.com>
2025-01-06 16:15:49 +0900

    車クラス作成課題

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,10 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		Car_Chapter15 car_chapter15 = new Car_Chapter15();
+		car_chapter15.gearChange(3);
+		car_chapter15.run();
+	}
+}
