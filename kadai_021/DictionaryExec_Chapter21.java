e5c4e88cbbb71e8b3eb265deb3d4d27dc32f2a2a
saliow <nemusak35@gmail.com>
2025-01-07 22:38:36 +0900

    辞書アプリを作ろう

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,11 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	public static void main(String[] args) {
+		Dictionary_Chapter21 myDictionary = new Dictionary_Chapter21();
+		
+		String[] wordsToCheck = {"apple","banana","grape","orange"};
+	
+		myDictionary.checkWords(wordsToCheck);
+	}
+}
